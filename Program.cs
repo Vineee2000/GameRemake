3c2b6a77a42c480d68b4407a213d6b094421835d
Vineee2000 <bogdan.vino@gmail.com>
2023-10-19 16:45:13 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Diagnostics;
+
+namespace GameLogic
+{
+    internal class Startup
+    {
+        static void Main(string[] args)
+        {
+            GameLoop gameLoop = new GameLoop();
+            while (true)
+            {
+                gameLoop.RunGame();
+            }
+        }
+    }
+
+    internal class GameLoop
+    {
+        private static int tickTime = 15;
+        private List<GameObject> gameObjectsList = new List<GameObject>();
+        internal void RunGame()
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            while (true)
+            {
+                foreach (var gameObject in gameObjectsList.ToList())
+                {
+                    gameObject.Update();
+                    if (gameObject.dead)
+                    {
+                        gameObjectsList.Remove(gameObject);
+                    }
+                }
+                Console.WriteLine("Tick");
+                if (sw.Elapsed < TimeSpan.FromMilliseconds(tickTime))
+                {
+                    Thread.Sleep((int)(tickTime - sw.ElapsedMilliseconds));
+                }
+                sw.Restart();
+            }
+        }
+    }
+    internal class GameObject
+    {
+        public bool dead { get; set; } = false;
+        public void Update()
+        {
+
+        }
+    }
+}
