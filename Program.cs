3a375807e8abc4ceeb68840a7ac3a02dbfe30cd9
Vineee2000 <bogdan.vino@gmail.com>
2023-10-26 02:01:10 +0100

    Created an empty form window and put it on a separate thread

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
-﻿using System.Diagnostics;
+﻿using GameRemake;
+using Graphics;
+using System.Diagnostics;
 using System.Drawing;
 using System.Numerics;
+using System.Windows.Forms;
 
 namespace GameLogic
 {
@@ -8,11 +11,10 @@ namespace GameLogic
     {
         static void Main(string[] args)
         {
+            Thread graphicsThread = new Thread(new ThreadStart(Graphics.GraphicsStartup.DisplayWindow));
+            graphicsThread.Start();
             GameLoop gameLoop = new GameLoop();
-            while (true)
-            {
-                gameLoop.RunGame();
-            }
+            gameLoop.RunGame();
         }
     }
 
@@ -54,12 +56,61 @@ namespace GameLogic
     {
         public Vector2 Velocity { get; set; }
 
-        public Point MoveBy(int x, int y);
-        public Vector2 ChangeVelocity(int x, int y);
+        public void MoveBy(int x, int y);
+        public void MoveBy(Vector2 movement);
+        public void ChangeVelocity(int x, int y);
+        public void ChangeVelocity(Vector2 velocity);
     }
 }
 
 namespace Graphics
 {
+    public class GraphicsStartup
+    {
+        public static void DisplayWindow()
+        {
+            GameWindow gameWindow = new GameWindow();
+            Application.Run(gameWindow);
+        }
+    }
+    public class GameWindow : Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
 
+        public GameWindow()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Text = "GameWindow";
+        }
+
+        #endregion
+    }
 }
